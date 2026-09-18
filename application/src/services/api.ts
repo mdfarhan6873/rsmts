9e93aac259f2a129c01d2d997795ee81329cddda
railmmt272 <railmmt272@gmail.com>
2026-09-18 10:18:00 +0530

    Update api.ts

--- application/src/services/api.ts
+++ application/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://rsmts.onrender.com';
+const API_URL = 'https://railsmts.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
